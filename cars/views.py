e676781823e285424d2c1e946e02b36508bf7892
Ravi <ravi92teja@gmail.com>
2019-11-07 17:40:07 +0530

    Modified the view car_list and beautified car_list.html

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
 def car_list(request):
-    return render(request, 'cars/car_list.html', {})
+    cars = Car.objects.all()
+    return render(request, 'cars/car_list.html', {'cars': cars})
