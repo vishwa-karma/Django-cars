1b2be35a1d862a308868504b94c04e77989e03d7
Ravi <ravi92teja@gmail.com>
2019-11-07 18:15:03 +0530

    Added a new URL car/id and car_detail, it's corresponding view and template

--- cars/urls.py
+++ cars/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.car_list, name='car_list')
+    path('', views.car_list, name='car_list'),
+    path('car/<int:pk>/', views.car_detail, name="car_detail"),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Car
 
 # Create your views here.
 def car_list(request):
     cars = Car.objects.all()
     return render(request, 'cars/car_list.html', {'cars': cars})
+
+def car_detail(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+    return render(request, 'cars/car_detail.html', {'car': car})
