569e30b6e16fc7cc85fbc131262a2ad830d862da
Ravi <ravi92teja@gmail.com>
2019-11-07 16:31:30 +0530

    Created Cars Model and made migrations

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django import forms
 
+MODEL_CHOICES = [("SUV", "Sports Utility Vehicle"),
+                    ("Sedan", "Sedan"),
+                    ("Hatchback", "Hatchback"),
+                    ("Mini SUV", "Mini Sports Utility Vehicle"),
+                    ("Sports Car", "Sports Car")
+                ]
+MAKE_CHOICES = [
+        ("Lamborghini", "Lamborghini"),
+        ("Alfa Romeo", "Alfa Romeo"),
+        ("Lexus", "Lexus"),
+        ("Cadillac", "Cadillac"),
+        ("Bentley", "Bentley"),
+        ("Range Rover", "Range Rover"),
+        ("Jaguar", "Jaguar"),
+        ("Renault", "Renault"),
+        ("Suzuki", "Maruti Suzuki"),
+        ("Mahindra", "Mahindra"),
+        ("Tata", "Tata"),
+        ("Fiat", "Fiat"),
+        ("Datsun", "Datsun"),
+        ("Ford", "Ford"),
+        ("Toyota", "Toyota"),
+        ("Honda", "Honda"),
+        ("Hyundai", "Hyundai"),
+        ("Maserati", "Maserati"),
+        ("Audi", "Audi"),
+        ("Volkswagon", "Volkswagon"),
+        ("BMW", "BMW"),
+        ("Mini", "Mini")
+    ]
+FUEL_CHOICES = [
+    ("Petrol", "Petrol"),
+    ("Diesel", "Diesel"),
+    ("Electric", "Electric"),
+    ("Hybrid", "Hybrid")
+]
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=200)
+    model_type = models.CharField(choices = MODEL_CHOICES, max_length=200)
+    make = models.CharField( choices = MAKE_CHOICES, max_length=200)
+    ftype = forms.ChoiceField(choices = FUEL_CHOICES, widget = forms.RadioSelect)
+    remarks = models.TextField()
+
+    def __str__(self):
+        return self.name
