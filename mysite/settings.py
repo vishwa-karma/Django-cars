ed1b71eded0b7c936654db3ee832fb76bdeffafc
Ravi <ravi92teja@gmail.com>
2019-11-07 20:31:09 +0530

    Created models.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'students.apps.StudentsConfig',
     'cars.apps.CarsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- students/models.py
+++ students/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+
+GENDER_LIST=[
+    ("Male", "Male"),
+    ("Female", "Female")
+]
+DEPARTMENT_LIST = [
+    ("Mech", "Mechanical Engineering"),
+    ("Civil", "Civil Engineering"),
+    ("CSE", "Computer Science Engineering"),
+    ("IT", "Information Technology"),
+    ("ECE", "Electronics & Communications Engineering"),
+    ("EEE", "Electrical & Electronics Engineering")
+]
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=10, choices=GENDER_LIST)
+    dob = models.DateField()
+    dept = models.CharField(max_length=50, choices=DEPARTMENT_LIST)
+    remarks = models.TextField
+
+    def __str__(self):
+        return self.name
