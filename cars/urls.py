4518d5a0d0de81aa77b2d5be06c827f387656322
Ravi <ravi92teja@gmail.com>
2019-11-19 13:57:04 +0530

    changed urls

--- cars/urls.py
+++ cars/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.car_list, name='car_list'),
+    path(' car/list/', views.car_list, name='car_list'),
     path('car/<int:pk>/', views.car_detail, name="car_detail"),
     path('car/new', views.new_car, name='new_car'),
     path('car/<int:pk>/edit/', views.car_edit, name='car_edit'),
--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.student_list, name='student_list'),
+    path('student/list/', views.student_list, name='student_list'),
     path('student/<int:pk>/', views.student_detail, name="student_detail"),
     path('student/new', views.new_student, name='new_student'),
     path('student/<int:pk>/edit/', views.student_edit, name='student_edit'),
